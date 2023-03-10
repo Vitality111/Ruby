794bbca4d75bdb58d1489a38bc6ac88869cad321
Vitalii <mirakleee@gmail.com>
2023-03-10 11:44:28 +0200

    add reject method

--- split_array.rb
+++ split_array.rb
@@ -6,3 +6,12 @@ def split_arr (array=(1..100).to_a)
     puts "Odd numbers: #{array2}"
 end
 splits_arr()
+
+# or
+
+array = (1..100).to_a
+array1 =[]
+array2 = []
+array.reject {|x| x.odd? ? array1 << x : array2 << x}
+puts array1 
+puts array2
